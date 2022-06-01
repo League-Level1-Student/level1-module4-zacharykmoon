c695b1f6397a830a7261799db378f612299f0a4d
JamesQian123 <jamesqian12345678910@gmail.com>
2022-06-01 16:57:36 -0700

    fff

--- src/_03_jeopardy/Jeopardy.java
+++ src/_03_jeopardy/Jeopardy.java
@@ -40,6 +40,7 @@ public class Jeopardy implements ActionListener {
 	private int score = 0;
 	private JLabel scoreBox = new JLabel("0");
 	private int buttonCount = 0;
+	private String answer;
 	
 
 
@@ -51,15 +52,17 @@ public class Jeopardy implements ActionListener {
 		frame.setLayout(new BorderLayout());
 
 		// 1. Make the frame show up
-
+frame.show();
 		// 2. Give your frame a title
-
+frame.setTitle("Jeoprady");
 		// 3. Create a JPanel variable to hold the header using the createHeader method
-
+JPanel header = createHeader("Dinos");
+quizPanel.add(header);
+frame.add(quizPanel);
 		// 4. Add the header component to the quizPanel
-
+firstButton=createButton("100"); 
 		// 5. Add the quizPanel to the frame
-
+quizPanel.add(firstButton);
 		// 6. Use the createButton method to set the value of firstButton
 
 		// 7. Add the firstButton to the quizPanel
@@ -71,11 +74,19 @@ public class Jeopardy implements ActionListener {
 		// method
 
 		// 10. Add the secondButton to the quizPanel
-
+secondButton=createButton("200");
+thirdButton=createButton("400");
+fourthButton=createButton("600");
+quizPanel.add(thirdButton);
+quizPanel.add(fourthButton);
+quizPanel.add(secondButton);
 		// 11. Add action listeners to the buttons (2 lines of code)
-
+firstButton.addActionListener(this);
+secondButton.addActionListener(this);
+thirdButton.addActionListener(this);
+fourthButton.addActionListener(this);
 		// 12. Write the code to complete the actionPerformed() method below
-
+ 
 		// 13. Add buttons so that you have $200, $400, $600, $800 and $1000 questions
 		
 		 /*
@@ -94,7 +105,11 @@ public class Jeopardy implements ActionListener {
 	private JButton createButton(String dollarAmount) {
 		
 		// Create a new JButton
+JButton Button = new JButton();
 
+button.setText(dollarAmount);
+buttonCount = buttonCount+1;
+return button;
 		// Set the text of the button to the dollarAmount
 
 		// Increment the buttonCount (this should make the layout vertical)
@@ -113,13 +128,23 @@ public class Jeopardy implements ActionListener {
 		// If the buttonPressed was the firstButton
 
 			// Call the askQuestion() method
- 
+ if(buttonPressed.equals(firstButton)) {
+	 askQuestion("Our planet is called Earth","Yes",100 );
+ }
 		// Complete the code in the askQuestion() method. When you play the game, the score should change.
 
 		// If the buttonPressed was the secondButton
-
+ if(buttonPressed.equals(secondButton)) {
+	 askQuestion("What planet has rings?","Saturn",200 );
+ }
 			// Call the askQuestion() method with a harder question
-
+ if(buttonPressed.equals(thirdButton)) {
+	 askQuestion("What is the closest planet to the sun?","Mercury",400 );
+ }
+ if(buttonPressed.equals(fourthButton)) {
+	 askQuestion("Which planet is a Gas Giant ","Uranus",600 );
+ }
+ 
 		// Clear the text on the button that was pressed (set the button text to nothing)
 
 	}
@@ -127,17 +152,25 @@ public class Jeopardy implements ActionListener {
 	private void askQuestion(String question, String correctAnswer, int prizeMoney) {
 		
 		// Use the playJeopardyTheme() method to play music while the use thinks of an answer
-		
+		playJeopardyTheme();
 		// Remove this temporary message and replace it with a pop-up that asks the user the question
 		JOptionPane.showMessageDialog(null, "this is where the question will be asked");
 		
 		// Stop the theme music when they have entered their response. Hint: use the sound variable 
-		
+		if(answer.equalsIgnoreCase(correctAnswer)) {
+			
+			 stopJeopardyTheme();
+		}
 		// If the answer is correct
-
+		else {
+			stopJeopardyTheme();
+			score=score-prizeMoney;
+			JOptionPane.showMessageDialog(null,"nope , it was"+correctAnswer+"your score has been decreased by" +prizeMoney);
+			updateScore();
+		}
 			// Increase the score by the prizeMoney
-
-			// Pop up a message to tell the user they were correct
+updateScore()
+			;// Pop up a message to tell the user they were correct
 
 		// Otherwise
 
@@ -148,6 +181,12 @@ public class Jeopardy implements ActionListener {
 		// Call the updateScore() method
 
 	}
+
+	private void stopJeopardyTheme() {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 	public static synchronized void playJeopardyTheme()
 	    {
