67b115bee82e60a6a065135686dd299506a7d3bb
league <league@imac-21.attlocal.net>
2022-08-17 18:25:58 -0700

    dddd

--- src/_12_slot_machine/SlotMachine.java
+++ src/_12_slot_machine/SlotMachine.java
@@ -3,7 +3,11 @@ package _12_slot_machine;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
+import java.util.Random;
 
+import javax.print.DocFlavor.URL;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -23,15 +27,15 @@ public class SlotMachine implements ActionListener {
 	
 	int wins = 0;
 	JTextArea t = new JTextArea("wins: " + wins);
-}
+
  public SlotMachine() throws MalformedURLException {
 
 	
 	frame.add(panel);
 
-	slot1 = createLabelImage("carror.png");
-	slot2 = createLabelImage("corn.wep");
-	slot3 = createLabelImage("pepper.jpeg");
+	slot1 = createLabelImage("carrot.png");
+	slot2 = createLabelImage("pepper.jpeg");
+	slot3 = createLabelImage("spidereye.png");
 
 	slot1.setSize(100, 100);
 	slot2.setSize(100, 100);
@@ -44,15 +48,55 @@ public class SlotMachine implements ActionListener {
 	button.addActionListener(this);
 	panel.add(t);
 	
+	frame.setVisible(true);
+	frame.pack();
+ }
 	
 	
+	public void spin() throws MalformedURLException {
+		
+		String[] files = {"carrot.png", "pepper.jpeg","spidereye.png"};
+		int ran1 = new Random().nextInt(3);
+		int ran2 = new Random().nextInt(3);
+		int ran3 = new Random().nextInt(3);
+		
+		
+		panel.remove(slot1);
+		panel.remove(slot2);
+		panel.remove(slot3);
+		
+		slot1= createLabelImage(files[ran1]);
+		slot2 = createLabelImage(files[ran2]);
+	    slot3 = createLabelImage(files[ran3]);
+	    
+	    
+	    panel.add(slot1);
+	    panel.add(slot2);
+	    panel.add(slot3);
+	    
+	
 }
-private JLabel createLabelImage(String string) {
-	// TODO Auto-generated method stub
-	return null;
+	private JLabel createLabelImage(String fileName) throws MalformedURLException{
+        java.net.URL imageURL = getClass().getResource(fileName);
+	if (imageURL == null){
+		System.err.println("Could not find image " + fileName);
+		return new JLabel();
+	}
+	Icon icon = new ImageIcon(imageURL);
+	JLabel imageLabel = new JLabel(icon);
+	return imageLabel;
 }
+
 @Override
 public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-	
+	if(e.getSource() == button) {
+		try {
+			spin();
+			frame.pack();
+		} catch (MalformedURLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+}
+}
 }
--- src/_12_slot_machine/SlotMachineRunner.java
+++ src/_12_slot_machine/SlotMachineRunner.java
@@ -1,5 +1,12 @@
 package _12_slot_machine;
 
+import java.net.MalformedURLException;
+
 public class SlotMachineRunner {
 
+	public static void main(String[] args) throws MalformedURLException {
+		
+		SlotMachine s = new SlotMachine();
+		
+	}
 }
